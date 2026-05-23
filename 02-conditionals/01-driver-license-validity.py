2c2e06c48dcd636f4a214a5ba87d7997022be0e9
Carolina Ezequiely <carolezequiely@hotmail.com>
2026-05-23 10:35:39 -0300

    Update 01-driver-license-validity.py

--- 02-conditionals/01-driver-license-validity.py
+++ 02-conditionals/01-driver-license-validity.py
@@ -4,13 +4,13 @@ age = int(input("What's your age? "))
 # License validity changes according to the driver's age
 # Conditions are checked from highest age to lowest age
 
-if age >= 116:
-    print("You broke a record! The oldest person currently is 116 years old! ")
-elif age >= 70:
-    print("Your driver's license validity is 3 years")
-elif age >= 50:
-    print("Your driver's license validity is 5 years")
-elif age >= 18:
-    print("Your driver's license validity is 10 years")
-else:
+if age < 18:
     print("You are not old enough to drive!")
+elif age < 50:
+    print("Your driver's license validity is 10 years")
+elif age < 70:
+    print("Your driver's license validity is 5 years")
+elif age < 116:
+    print("Your driver's license validity is 3 years")
+else:
+    print("You broke a record! The oldest person currently is 116 years old! ")
